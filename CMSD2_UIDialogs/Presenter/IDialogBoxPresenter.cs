9ab1edfa6b8901763d1da0d20aa383952ccdd82f
Sabir Aliyev <Sabir Aliyev@azfna.local>
2019-06-08 09:03:51 +0400

    First commit of 0.0.0.0 version

---
+++ CMSD2_UIDialogs/Presenter/IDialogBoxPresenter.cs
@@ -0,0 +1,14 @@
+﻿using DocumentOrganizer_2.UIDialogs.ViewModelService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentOrganizer_2.UIDialogs.Presenter
+{
+    public interface IDialogBoxPresenter<T> where T : IDialogViewModel
+    {
+        void Show(T viewModel);
+    }
+}
